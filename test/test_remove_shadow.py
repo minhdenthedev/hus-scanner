b73e494bf43c23247103fd3da0a52502beac4062
minhdenthedev <minhden.dev@gmail.com>
2024-11-27 00:39:01 +0700

    add contour detection

--- test/test_remove_shadow.py
+++ test/test_remove_shadow.py
@@ -7,15 +7,31 @@ from src.binarizer.remove_shadow import RemoveShadow
 from src.pipeline import Pipeline
 from src.utils import detect_corner
 
-img = cv.imread("../test_images/IMG_8511.jpg", 0)
+image = cv.imread("../test_images/IMG_8511.jpg")
+gray = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
+kernel = np.ones((7,7),np.uint8)
+img = cv.morphologyEx(gray, cv.MORPH_CLOSE, kernel, iterations= 3)
+
+# plt.imshow(img, cmap="gray")
 
 pipeline = Pipeline(stages=[
     RemoveShadow(),
     Binarizer()
 ])
 
-output = pipeline.execute(img)
+img = pipeline.execute(img)
 
-corners = detect_corner(img)
-print(corners)
+contours, hierarchy = cv.findContours(img, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+min_contour_area = 500  # Ngưỡng diện tích, có thể điều chỉnh tùy theo ảnh của bạn
+large_contours = [cnt for cnt in contours if cv.contourArea(cnt) > min_contour_area]
+large_contours = sorted(large_contours, key=cv.contourArea, reverse=True)
+for i in range(len(large_contours[0])):
+    if 0 in large_contours[0][i][0]:
+        large_contours.pop(0)
+
+contoured_image = image.copy()
+cv.drawContours(contoured_image, large_contours, -1, (0, 255, 0), 30)
+
+plt.imshow(contoured_image)
+plt.show()
 
