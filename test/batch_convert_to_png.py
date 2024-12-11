f76e69c2e61b84055829f4cf7919217e32f610b8
hausura <hanampy@gmail.com>
2024-12-11 09:14:01 +0700

    connect finding best corner, warping

--- test/batch_convert_to_png.py
+++ test/batch_convert_to_png.py
@@ -1,3 +1,3 @@
 from src.utils import batch_convert_to_png
 
-batch_convert_to_png('../test_images/new_HEIC_images', '../test_images/unfiltered_pngs')
+batch_convert_to_png('./test_images/new_HEIC_images', './test_images/unfiltered_pngs')
